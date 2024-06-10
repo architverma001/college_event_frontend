01f823e8348a3425b368a390f5ae84fd8226a30a
Kunj Detroja <kunjdetroja52@gmail.com>
2024-06-10 21:21:58 +0530

    Top Colleges Table Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,19 @@ import Navbar from './components/Navbar';
 import SwipwerSlides from './components/SwiperSlides';
 import HomePage from './pages/homepage/HomePage';
 import Footer from './components/footer/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import TopColleges from './pages/TopColleges';
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
       <SwipwerSlides/>
       <HomePage/>
+      <TopColleges/>
 
       <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
