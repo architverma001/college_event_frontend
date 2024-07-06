bc7bdf01f791ed875223a21bb72c3c6ce3f8477e
Kunj Detroja <kunjdetroja52@gmail.com>
2024-07-06 19:48:04 +0530

    Backend Authentication file added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const MONGO_URI = process.env.MONGO_URI ;
-const MONGO_DB = process.env.MONGO_DB;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI + MONGO_DB);
+    await mongoose.connect(MONGO_URI);
     console.log("MongoDB connection SUCCESS");
   } catch (error) {
     console.error(error);
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 const cors = require("cors");
-const connectDB = require("./db");
+const connectDB = require("./config/db");
 
 const collegeRoutes = require("./routes/college.route");
 const courseRoutes = require("./routes/course.route");
 const cutoffsRoutes = require("./routes/cutoffs.route");
 const chatRoutes = require('./routes/chatRoutes');
+const healthCheckRoutes = require('./routes/healthcheck.route');
+const authRoutes = require('./routes/auth.route');
 
 connectDB();
 
@@ -20,6 +22,8 @@ app.use("/colleges", collegeRoutes);
 app.use("/courses", courseRoutes);
 app.use("/cutoffs", cutoffsRoutes);
 app.use('/api', chatRoutes);
+app.use('/healthcheck', healthCheckRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
